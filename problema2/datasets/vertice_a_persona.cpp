7faa9e5456e12d5dccd79be8fa057c74d9ae1d84
dani <juan.alegria@usm.cl>
2022-07-12 17:08:03 -0400

    "agregada la funcion vertice_a_persona"

---
+++ problema2/datasets/vertice_a_persona.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+string* id_array (int size){
+
+    string* id_array;
+    id_array = new string [size];
+    return id_array;
+}
+
+void asignar_nombres (string* id_array, int size){
+
+    ifstream file;
+    file.open("miserables_id_to_names.csv");
+
+    if (!file.is_open()){
+        cout << "Error al abrir el archivo." << endl;
+        exit(1);
+    }
+
+    string line;
+    getline(file,line);
+
+    for (int i = 0 ; i < size ; i++){
+
+        getline(file, line);
+        char* new_array = new char[line.length()];
+        strcpy(new_array, line.c_str());
+
+        char* p;
+
+        p = strtok(new_array, ",");
+        int j = stoi(p);
+        p = strtok(NULL, ",");
+        string name(p);
+        id_array[j] = name;
+        delete [] new_array;
+    }
+}
+
+int main(){
+
+    cout << "introduce el size: " << endl;
+    int size;
+    cin >> size;
+
+    string* p = id_array(size);
+
+    asignar_nombres(p, size);
+    return 0;
+}
